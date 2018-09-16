ec951dacc70d213ec2936cb459b94c40a850dfd4
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-16 12:37:05 -0500

    Add project files.

---
+++ CsharpLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
